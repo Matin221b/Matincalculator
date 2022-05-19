a5d9053cb85f1fd2c46e101150a429123841de3b
mahdi221b <mahdi221b221m@gmail.com>
2022-05-19 08:40:26 -0700

    اضافه کردن دکمه های ماشین حساب

--- matincalculator/src/components/HelloWorld.vue
+++ matincalculator/src/components/HelloWorld.vue
@@ -11,11 +11,39 @@ export default {
 
 <template>
   <h1>{{ msg }}</h1>
+
+<table dir="rtl" id="btn">
+<tr><input dir="rtl" type="number" id="display"></tr>
+<tr>
+ <td><button id="div">/</button></td>
+ <td><button id="clear">C</button></td>
+ <td><button id="delete">del</button></td>
+</tr>
+<tr>
+ <td><button id="mul">*</button></td>
+ <td><button id="digit">9</button></td>
+ <td><button id="digit">8</button></td>
+ <td><button id="digit">7</button></td>
+</tr>
+<tr>
+ <td><button id="minus">-</button></td>
+ <td><button id="digit">6</button></td>
+ <td><button id="digit">5</button></td>
+ <td><button id="digit">4</button></td>
+</tr>
+<tr>
+ <td><button id="sum">+</button></td>
+ <td><button id="digit">3</button></td>
+ <td><button id="digit">2</button></td>
+ <td><button id="digit">1</button></td>
+</tr>
+<tr>
+ <td><button id="point">.</button></td>
+ <td><button id="digit">0</button></td>
+ <td><button id="equal">=</button></td>
+</tr>
+</table>
 </template>
 
 <style>
-a {
-  color: #42b983;
-
-}
 </style>
