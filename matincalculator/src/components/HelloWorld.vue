8cd7fb1beb5f771328192e67736b8ba81c87f9af
mahdi221b <mahdi221b221m@gmail.com>
2022-05-23 00:05:24 -0700

    تغییرات جدید ظاهری در ماشین حساب

--- matincalculator/src/components/HelloWorld.vue
+++ matincalculator/src/components/HelloWorld.vue
@@ -1,12 +1,4 @@
 <script>
-export default {
-  props: {
- 
-  },
- 
-  methods: {
-  }
-}; 
 </script>
 
 <template>
@@ -20,10 +12,9 @@ export default {
       
       </tr>
       <tr>
-        <td><button @click="clear" id="clear">C</button></td>
-        <td><button @click="del" id="delete">del</button></td>
-        <td><button @click="point()" id="point" class="point">.</button></td>
+        <td><button @click="mul" id="mul">*</button></td>
         <td><button @click="mod" id="mod">%</button></td>
+        <td colspan="2"><button @click="clear" id="clear">C</button></td>
       </tr>
       <tr>
         <td><button @click="div" id="div">/</button></td>
@@ -32,21 +23,20 @@ export default {
         <td><button @click="digit(7)" id="digit" class="digit">7</button></td>
       </tr>
       <tr>
-        <td><button @click="mul" id="mul">*</button></td>
+        <td><button @click="minus" id="minus">-</button></td>
         <td><button @click="digit(6)" id="digit" class="digit">6</button></td>
         <td><button @click="digit(5)" id="digit" class="digit">5</button></td>
         <td><button @click="digit(4)" id="digit" class="digit">4</button></td>
       </tr>
       <tr>
-        <td><button @click="minus" id="minus">-</button></td>
+        <td><button @click="sum" id="sum">+</button></td>
         <td><button @click="digit(3)" id="digit" class="digit">3</button></td>
         <td><button @click="digit(2)" id="digit" class="digit">2</button></td>
         <td><button @click="digit(1)" id="digit" class="digit">1</button></td>
       </tr>
       <tr>
         <td colspan="2"><button @click="equal" id="equal">=</button></td>
-        <td><button @click="digit(0)" id="digit" class="digit">0</button></td>
-        <td><button @click="sum" id="sum">+</button></td>
+        <td colspan="2"><button @click="digit(0)" id="digit" class="digit">0</button></td>
       </tr>
     </table>
   </div>
@@ -68,24 +58,11 @@ text-align: right
   width: calc(100%);
   padding: 10px 0;
 }
-#delete {
-  background-color: rgb(210, 140, 89);
-  border: 1px solid rgb(210, 140, 89);
-  width: calc(100%);
-  padding-bottom: 10px;
-  padding-top: 10px;
-}
 #digit{
   background-color: rgb(255, 255, 255);
   border: 1px solid rgb(0, 0, 0);
   width: calc(100%);
   padding: 10px;
-}
-#point {
-  background-color: rgb(201, 113, 255);
-  border: 1px solid rgb(201, 113, 255);
-  width: calc(100%);
-  padding: 10px
 }
 #sum {
   background-color: rgb(210, 89, 200);
